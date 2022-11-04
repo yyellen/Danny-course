dc9af33b76bcb14d71ea8e715b32e93adabbe213
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-11-04 17:50:04 +0800

    Create 4.triangle.js

---
+++ flow control assignment/4.triangle.js
@@ -0,0 +1,15 @@
+// 三角形
+
+function triangle(size) {
+  let star = "";
+  for (let i = 1; i <= size; i++) {
+    star += "*"
+    console.log(star)
+    // for (let j = 1; j <= i; j++) {
+    // }
+    // console.log("\n")
+  }
+}
+
+triangle(5);
+triangle(12);
