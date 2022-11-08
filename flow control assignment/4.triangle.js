97e181742164d3ace01cf3dff29eb932aa65fc22
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-11-08 13:56:25 +0800

    Update 4.triangle.js

--- flow control assignment/4.triangle.js
+++ flow control assignment/4.triangle.js
@@ -1,15 +1,94 @@
 // 三角形
 
 function triangle(size) {
-  let star = "";
+  let str = "";
   for (let i = 1; i <= size; i++) {
-    star += "*"
-    console.log(star)
-    // for (let j = 1; j <= i; j++) {
-    // }
-    // console.log("\n")
+    str += "*";
+    console.log(str);
   }
 }
 
 triangle(5);
 triangle(12);
+
+// *
+// **
+// ***
+// ****
+// *****
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
+// ************
+
+// 學到另一種寫法-repeat
+function triangleRepeat(size) {
+  for (let i = 1; i <= size; i++) {
+    let star = "*";
+    console.log(`${star.repeat(i)}`);
+  }
+}
+
+triangleRepeat(5);
+triangleRepeat(12);
+
+// *
+// **
+// ***
+// ****
+// *****
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
+// ************
+
+
+// 另一種寫法-Nested loop
+function triangleNested(size) {
+  let strr = "";
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= i; j++) {
+      strr += "*";
+    }
+    strr += "\n";
+  }
+  console.log(strr);
+}
+
+triangleNested(5);
+triangleNested(12);
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+// ***********
+// ************
