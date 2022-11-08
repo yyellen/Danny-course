7d0f6b6eb10d874658f586b50816c84b1177289a
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-11-08 13:54:03 +0800

    Update 1.odd-or-even-number.js

--- flow control assignment/1.odd-or-even-number.js
+++ flow control assignment/1.odd-or-even-number.js
@@ -24,3 +24,30 @@ numOddEven(-2); // -2是偶數
 numOddEven(-100000); // -100000是偶數
 numOddEven(-1111111); // -1111111是奇數
 numOddEven(0.1); // 無法判斷(小數沒有奇數偶數之分)
+
+
+// 另一種寫法
+function oddOrEven(num) {
+  const result = (num % 2) === 0 ? "偶數" : "奇數";
+  console.log(num + result);
+}
+
+oddOrEven(0);
+oddOrEven(1);
+oddOrEven(2);
+oddOrEven(-1);
+oddOrEven(100000);
+oddOrEven(1111111);
+oddOrEven(-100000);
+oddOrEven(-1111111);
+oddOrEven(0.1);
+
+// 0偶數
+// 1奇數
+// 2偶數
+// -1奇數
+// 100000偶數
+// 1111111奇數
+// -100000偶數
+// -1111111奇數
+// 0.1奇數
