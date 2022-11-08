c8e1791ef247ddc7101d7dd90328945c979bdd39
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-11-08 13:54:13 +0800

    Update 3.multiplication-table.js

--- flow control assignment/3.multiplication-table.js
+++ flow control assignment/3.multiplication-table.js
@@ -1,8 +1,8 @@
 // 九九乘法表
 
-for (let i = 1; i < 10; i++) {
-  for (let j = 1; j < 10; j++) {
-    console.log(i +" x "+ j + " = " + i*j)
+for (let i = 1; i <= 9; i++) {
+  for (let j = 1; j <= 9; j++) {
+    console.log(`${i} x ${j} = ${i*j}`)
   }
 }
 
