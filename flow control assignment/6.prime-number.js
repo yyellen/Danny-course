521300888060d5cb6349e8e0c64acaf5a64873b6
yyellen <yyopenfolder@gmail.com>
2022-11-11 10:33:28 +0800

    Update 6.prime-numbers

--- flow control assignment/6.prime-number.js
+++ flow control assignment/6.prime-number.js
@@ -1,20 +1,53 @@
 // 質數 給定一個數，顯示出從 2 至此數所有的質數 給 11 要顯示 2, 3, 5, 7, 11
-// 印不出2 多印出9
-function isPrime(n) {
-    for (let i = 2; i <= n; i++) {
-      if ((n % i) == 0) {
-        return false;
-      }
-      return true;
-    }
-}
 
-function getPrime(num) {
-  for (let j = 2; j <= num; j++) {
-    if (isPrime(j)) {
-      console.log(j);
+// 老師解答
+function primeNumbers(num) {
+  let result = [];
+  for (let i = 2; i < num; i++) {
+    let isPrime = true;
+    if (i === 2) {
+      result.push(2);
+    } else { for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        // 這個數字不是質數
+        isPrime = false;
+      }
+    }}
+    if (isPrime && i != 2) {
+      result.push(i);
     }
   }
+  
+  console.log(result);
 }
 
-getPrime(11);
+primeNumbers(11); // [ 2, 3, 5, 7 ]
+primeNumbers(1); // []
+// primeNumbers(10000);
+
+
+// // 變成印奇數..
+// function isPrime(n) {
+//     for (let i = 2; i <= n; i++) {
+//       if ((n % i) == 0) {
+//         return false;
+//       }
+//       return true;
+//     }
+// }
+
+// function getPrime(num) {
+//   for (let j = 2; j <= num; j++) {
+//     if (isPrime(j)) {
+//       console.log(j);
+//     }
+//   }
+// }
+
+// getPrime(11);
+
+// 3
+// 5
+// 7
+// 9
+// 11
