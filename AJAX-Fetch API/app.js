aa1f7d7256b78898bb67e9702371f6cdcf3e1f59
yyellen <yyopenfolder@gmail.com>
2022-12-23 15:24:04 +0800

    Feat: 新增畫面&DOM操作行為

--- AJAX-Fetch API/app.js
+++ AJAX-Fetch API/app.js
@@ -1,11 +1,42 @@
+  const input = document.querySelector('.input')
+  const findButton = document.querySelector('.find-button')
+  const result = document.querySelector('.result')
+
 // https://www.geojs.io/docs/v1/endpoints/country/
 // https://developer.mozilla.org/zh-TW/docs/Web/API/Fetch_API/Using_Fetch
 
-fetch('https://get.geojs.io/v1/ip/country?ip=8.8.8.8')
-  .then(function(response) {
-    return response.text();
+findButton.addEventListener('click', () => {
+    const inputValue = input.value
+    // console.log(inputValue);
+  
+    const url = `https://get.geojs.io/v1/ip/country?ip=${inputValue}`
+  
+    fetch(url)
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      result.innerHTML = data
+    })
   })
-  .then(function(result) {
-    console.log(result);
-  });
+
+// 1216-自己寫的作業
+  // const input = document.querySelector('.input')
+  // const findButton = document.querySelector('.find-button')
+  // const result = document.querySelector('.result')
+  
+  // findButton.addEventListener('click', () => {
+  //   const inputValue = input.value
+  //   // console.log(inputValue);
+  
+  //   const url = `https://get.geojs.io/v1/ip/country?ip=${inputValue}`
+  
+  //   fetch(url)
+  //   .then((response) => response.text())
+  //   .then((data) => {
+  //     // console.log(data)
+  //     result.innerHTML = data
+  //   })
+  //   .catch((error) => console.log(error));
+  // })
   
